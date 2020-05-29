8fd87059fe22b6b62189a036cbd15daec2f5bd59
Gustavo Bacagine <gustavo.bacagine@protonmail.com>
2020-05-29 16:09:08 -0300

    20.05.29v2

--- src/covid19.c
+++ src/covid19.c
@@ -77,7 +77,7 @@ total_contagios_pais(int num_days,
         /* Atribui zero toda vez 
          * que entra aqui */
         total_contagions_coutry = 0;
-        for(int j = 0; j <= num_days; j++){
+        for(int j = 0; j < num_days; j++){
             // Soma o total de contágios de cada país
             total_contagions_coutry += mat_total_contagions_pais[i][j];
         }
@@ -113,51 +113,37 @@ dia_maior_contagio_pais(int num_days,
         }
     }
 } // Até aqui tudo OK =)
-// NUNCA MAIS ALTERE ESTÁ FUNÇÃO
-// O PROBLEMA NÃO ESTÁ AQUI
-// MAS SIM NA FUNÇÃO -> total_contagios_pais
+
+/* NUNCA MAIS ALTERE ESTÁ FUNÇÃO
+ * O PROBLEMA NÃO ESTÁ AQUI
+ * MAS SIM NA FUNÇÃO -> total_contagios_pais */
 void 
 dia_maior_contagio(int num_countrys, 
                    int num_days, 
                    int mat_contagions[num_countrys][num_days], 
                    int *max_contagion_day, 
                    int *max_contagion_number){
-    /*
-    *max_contagion_number = 0;
-    for(int j = 0; j < num_days; j++){
-        for(int i = 0; i < num_countrys; i++){
-            *max_contagion_number += mat_contagions[i][j];
-            *max_contagion_day = j;
-        }
-    }*/
     
     *max_contagion_number = 0;
     *max_contagion_day = 0;
-    int soma_contagions_day = 0;
     int soma_maior = 0;
     int day = 0;
     
     for(int j = 0; j < num_days; j++){
         *max_contagion_day = j+1;
-        soma_contagions_day = 0;
+        *max_contagion_number= 0;
         for(int i = 0; i < num_countrys; i++){
-//            *max_contagion_number += mat_contagions[i][j];
-            soma_contagions_day += mat_contagions[i][j];
-            if(soma_contagions_day > soma_maior){
-//                printf("\nEntrei no if, valor de number: %d valor de day: %d", *max_contagion_number, *max_contagion_day);
-                soma_maior = soma_contagions_day;
+            *max_contagion_number += mat_contagions[i][j];
+            if(*max_contagion_number > soma_maior){
+                soma_maior = *max_contagion_number;
                 day = *max_contagion_day;
-//                printf("\nSaindo do if, valor de number: %d valor de day: %d", *max_contagion_number, *max_contagion_day);
             }
-/*            else if(mat_contagions[i][j] >= *max_contagion_number){
-                printf("\nEntrei no else if, valor de number: %d valor de day: %d", *max_contagion_number, *max_contagion_day);
-                printf("\nSaindo no else if, valor de number: %d valor de day: %d", *max_contagion_number, *max_contagion_day);
-            }*/
         }
     }
     
     *max_contagion_number = soma_maior;
     *max_contagion_day = day;
+    
 }
 
 int 
@@ -237,3 +223,12 @@ maior_sequencia_crescente(int num_days,
             }
         }
     } */
+
+    /*
+    *max_contagion_number = 0;
+    for(int j = 0; j < num_days; j++){
+        for(int i = 0; i < num_countrys; i++){
+            *max_contagion_number += mat_contagions[i][j];
+            *max_contagion_day = j;
+        }
+    }*/
